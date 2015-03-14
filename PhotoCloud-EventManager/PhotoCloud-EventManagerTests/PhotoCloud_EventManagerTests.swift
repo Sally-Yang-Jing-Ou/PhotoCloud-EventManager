0a4ddc892759ee97436bff57c701705ea79a2ec9
Sally-Yang-Jing-Ou <sally_auyoung@hotmail.com>
2015-03-14 13:01:02 -0400

    initial commit

---
+++ PhotoCloud-EventManager/PhotoCloud-EventManagerTests/PhotoCloud_EventManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoCloud_EventManagerTests.swift
+//  PhotoCloud-EventManagerTests
+//
+//  Created by Sally Yang Jing Ou on 2015-03-14.
+//  Copyright (c) 2015 Sally Yang Jing Ou. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PhotoCloud_EventManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
