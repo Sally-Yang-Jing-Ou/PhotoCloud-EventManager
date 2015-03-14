092ccd214f5e90de8642bd6c72fa0d516ff68464
Sally-Yang-Jing-Ou <sally_auyoung@hotmail.com>
2015-03-14 15:24:08 -0400

    [David/Sally] David said it's pretty good. Sign in/ All events

---
+++ PhotoCloud-EventManager/PhotoCloud-EventManager/AllEventsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AllEventsViewController.swift
+//  PhotoCloud-EventManager
+//
+//  Created by Sally Yang Jing Ou on 2015-03-14.
+//  Copyright (c) 2015 Sally Yang Jing Ou. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AllEventsViewController: UIViewController {
+    
+}
---
+++ PhotoCloud-EventManager/PhotoCloud-EventManager/SignInViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SignInViewController.swift
+//  PhotoCloud-EventManager
+//
+//  Created by Sally Yang Jing Ou on 2015-03-14.
+//  Copyright (c) 2015 Sally Yang Jing Ou. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    @IBOutlet weak var submitButton: UIButton?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func submitButton (sender: UIButton!){
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        
+        let allEventsViewController = storyboard?.instantiateViewControllerWithIdentifier("AllEventsViewController") as AllEventsViewController
+
+        self.presentViewController(allEventsViewController, animated: true, completion: nil)
+    }
+
+
+}
+
