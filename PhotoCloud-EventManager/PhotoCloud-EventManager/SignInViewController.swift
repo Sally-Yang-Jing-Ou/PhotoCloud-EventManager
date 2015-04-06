943a9f36c2848dd24d2a19c4305143271fc1b4ff
Sally-Yang-Jing-Ou <sally_auyoung@hotmail.com>
2015-04-05 23:11:28 -0400

    Password setup NSUserdefault

--- PhotoCloud-EventManager/PhotoCloud-EventManager/SignInViewController.swift
+++ PhotoCloud-EventManager/PhotoCloud-EventManager/SignInViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class SignInViewController: UIViewController {
-    @IBOutlet weak var submitButton: UIButton?
+class SignInViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var okayButton: UIButton?
+    @IBOutlet weak var passwordTextField: UITextField?
+    
+    let passwordConstant = "passwordKey"
+    var readPassword: String?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        readPassword = NSUserDefaults.standardUserDefaults().stringForKey(passwordConstant)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        readPassword = NSUserDefaults.standardUserDefaults().stringForKey(passwordConstant)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,13 +32,33 @@ class SignInViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func submitButton (sender: UIButton!){
-        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+    @IBAction func okayButton (sender: UIButton!){
+        var passwordText = passwordTextField?.text
         
-        let mainNavigationVC = storyboard?.instantiateViewControllerWithIdentifier("MainNavigationVC") as UINavigationController
+        if (readPassword == nil){
+            if (passwordText != "") {
+                NSUserDefaults.standardUserDefaults().setObject(passwordText, forKey: passwordConstant)
+                NSUserDefaults.standardUserDefaults().synchronize()
+            }
+            else {
+                var alert = UIAlertView(title: "Set Up Password", message: "Please set up your password", delegate: nil, cancelButtonTitle: "Cancel")
+                alert.show()
+            }
+            
+        } else if (passwordText == readPassword) {
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            
+            let mainNavigationVC = storyboard?.instantiateViewControllerWithIdentifier("MainNavigationVC") as UINavigationController
+            
+            self.presentViewController(mainNavigationVC, animated: true, completion: nil)
+        } else {
+            var alert = UIAlertView(title: "Wrong Password!", message: "Please enter the correct password", delegate: nil, cancelButtonTitle: "Cancel")
+            alert.show()
+        }
 
-        self.presentViewController(mainNavigationVC, animated: true, completion: nil)
-    }
+        
+        
+        }
 
 
 }
